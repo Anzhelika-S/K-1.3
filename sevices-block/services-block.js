3b6da8d52808cc143adcb983e66baf50f9624848
Anzhelika <anzhelikasilkina@yandex.ru>
2024-09-20 23:20:31 +0400

    add swiper function

--- sevices-block/services-block.js
+++ sevices-block/services-block.js
@@ -1,18 +1,1 @@
-import Swiper from "swiper";
-import { Pagination } from "swiper/modules";
-
-import "swiper/css";
-import "swiper/css/pagination";
-
-const swiper = new Swiper(".swiper", {
-  modules: [Pagination],
-
-  direction: "horizontal",
-  loop: true,
-
-  slidesPerView: 3,
-
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
+new Swiper('.swiper');
