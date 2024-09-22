8836ac6bb05e0d62b75b936096ff5a255727b71c
Anzhelika <anzhelikasilkina@yandex.ru>
2024-09-22 14:26:43 +0300

    add styles to swiper

--- sevices-block/services-block.js
+++ sevices-block/services-block.js
@@ -1,1 +1,67 @@
-new Swiper('.swiper');
+const mobile = window.matchMedia("(max-width:320px)");
+const desktop = window.matchMedia("(min-width:1220px)");
+
+const showMoreButton = document.querySelector(".button");
+const showMore = document.querySelector(".button__text--show");
+const hide = document.querySelector(".button__text--hide");
+const buttonArrows = document.querySelector(".button__arrows");
+const servicesSwiper = document.querySelector(".services__swiper");
+
+let init = false;
+let swiper = Swiper;
+
+function swiperMode() {
+  if (mobile.matches) {
+    if (!init) {
+      init = true;
+      swiper = new Swiper(".swiper", {
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+
+        grabCursor: true,
+
+        slidesPerView: 1,
+        spaceBetween: 16,
+
+        centeredSlides: true,
+        centeredSlidesBounds: true,
+      });
+    }
+  } else {
+    swiper.destroy();
+    init = false;
+  }
+}
+
+function showMoreLess() {
+  if (showMore.classList.contains("hidden")) {
+    showMore.classList.remove("hidden");
+    hide.classList.add("hidden");
+
+    buttonArrows.style.transform = "rotate(0deg)";
+    servicesSwiper.style.height = "200px";
+  } else {
+    showMore.classList.add("hidden");
+    hide.classList.remove("hidden");
+
+    buttonArrows.style.transform = "rotate(180deg)";
+
+    if (!desktop.matches) {
+      servicesSwiper.style.height = "380px";
+    } else {
+      servicesSwiper.style.height = "280px";
+    }
+  }
+}
+
+showMoreButton.addEventListener("click", showMoreLess);
+
+window.addEventListener("load", function () {
+  swiperMode();
+});
+
+window.addEventListener("resize", function () {
+  swiperMode();
+});
